c4a34a1f62ffe4c1c707642ee558b93155d33775
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-07-31 19:41:07 +0000

    Change Ruby Version for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
